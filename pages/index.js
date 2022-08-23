9eb2a15f9879d58655fc6c02053c8bac4b265600
CetO-hub <skynet@SkyNet.localdomain>
2022-08-23 17:21:06 +0200

    Adjust responsive styling for index page

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home(props) {
       plot: "",
     },
   ]);
-  console.log(dataMovie);
+
   useEffect(() => {
     if (props.movieDetails) {
       console.log("running");
@@ -45,8 +45,6 @@ export default function Home(props) {
       setMessage("");
     }
   }, [props]);
-
-  console.log(dataMovie);
 
   function handleOnChange(e) {
     const { name, value } = e.target;
@@ -85,18 +83,20 @@ export default function Home(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex flex-col items-center">
-        <div className="h-[200px] bg-hero bg-no-repeat bg-[center_top_-12rem] w-full relative flex">
+        <div className="h-[200px] bg-hero bg-no-repeat bg-[center_top_-12rem] w-full md:bg-cover md:bg-[center_top_-40rem] md:h-[300px] lg:h-[400px] relative flex ">
           <div className="absolute w-full h-full bg-black/50"></div>
-          <div className="px-8 w-full absolute top-[50%] translate-y-[-50%] z-10 text-white">
-            <div className="flex items-center justify-between w-full gap-8 font">
-              <h1 className="text-3xl font-roboto font-bold">Find your film</h1>
+          <div className="flex md:max-w-[70%] justify-between lg:max-w-[35%] px-8 md:px-0 w-full absolute top-[50%] translate-y-[-50%] md:left-[50%] md:translate-x-[-50%] z-10 text-white">
+            <div className="flex items-center justify-between md:justify-between w-full gap-8 md:gap-0 font md:px-8">
+              <h1 className="text-3xl md:text-5xl font-roboto font-bold">
+                Find your film
+              </h1>
               <Link href="/watchlist">
-                <a className="text-xs font-roboto">My Watchlist</a>
+                <a className="text-xs md:text-xl font-roboto">My Watchlist</a>
               </Link>
             </div>
           </div>
         </div>
-        <form className="-mt-4 w-full z-20 px-8">
+        <form className="-mt-4 w-full md:max-w-[70%]  lg:max-w-[35%] z-20 px-8">
           <div className="flex justify-between border-2 border-gray-400 rounded-lg bg-white w-full h-full">
             <label htmlFor="search" className="px-2">
               <AiOutlineSearch className="inline"></AiOutlineSearch>
@@ -108,7 +108,7 @@ export default function Home(props) {
               id="search"
               type="text"
               placeholder="Search for a movie"
-              className="ml-2 focus:outline-none "
+              className="ml-2 focus:outline-none w-full"
             />
             <button
               onClick={handleSubmit}
@@ -149,7 +149,7 @@ export async function getServerSideProps({ query }) {
         },
       };
     }
-    console.log(data);
+
     const movieIDArray = data.Search.map((movie) => movie.imdbID);
 
     const movieDetails = await Promise.all(
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -15,47 +15,54 @@ const Movies = (props) => {
     }
     return (
       <>
-        <div className="flex relative h-[150px] w-[100px]">
-          {movie.poster !== "N/A" ? (
-            <Image
-              key={movie.imbdID}
-              src={movie.poster}
-              layout="fill"
-              objectFit="cover"
-            />
-          ) : (
-            <h2 className="text-center self-center">No picture available</h2>
-          )}
-        </div>
-        <div className="flex ">
-          <div className="p-4 flex flex-col gap-y-3">
-            <div className="font-roboto flex justify-between">
-              <h2 className="inline mr-4 font-bold">{movie.title}</h2>
+        <div className="flex justify-between gap-5 items-center md:justify-start">
+          <div className="flex shrink-0 relative h-[150px] w-[100px]">
+            {movie.poster !== "N/A" ? (
+              <Image
+                key={movie.imbdID}
+                src={movie.poster}
+                layout="fill"
+                objectFit="cover"
+              />
+            ) : (
+              <h2 className="text-center self-center">No picture available</h2>
+            )}
+          </div>
+          <div className=" flex flex-col gap-y-3">
+            <div className="font-roboto flex justify-between md:justify-start">
+              <h2 className="inline mr-4 font-bold md:text-xl">
+                {movie.title}
+              </h2>
               <h3 className="inline whitespace-nowrap">
-                <AiFillStar color="gold" className="inline" />
+                <AiFillStar color="gold" className="mr-2 inline" />
                 {movie.imdbRating}
               </h3>
             </div>
-            <div className=" flex font-roboto gap-2 justify-between">
+            <div className="text-xs flex font-roboto gap-2 justify-between md:justify-start md:text-lg">
               <h2 className="inline shrink-0">{movie.runtime}</h2>
               <h3 className="inline ml-4">{movie.genre}</h3>
-              <h3 className="inline whitespace-nowrap">
-                <AiFillPlusCircle size={25} className="inline" />
+              <a className="inline whitespace-nowrap cursor-pointer h-fit">
+                <AiFillPlusCircle size={20} className="inline" />
                 Watchlist
-              </h3>
+              </a>
             </div>
             <div className="font-roboto">
-              <h2 className="inline text-gray-500">{text()}</h2>
+              <h2 className="text-sm md:text-lg inline text-gray-500">
+                {text()}
+              </h2>
             </div>
           </div>
         </div>
-        <div className="border-b-2 border-gray-400/50"></div>
+        <div className="border-b-2 w-full border-gray-400/50"></div>
       </>
     );
   });
+
   return (
     <>
-      <div className="flex flex-col gap-5 p-8 items-center">{movieCard}</div>
+      <div className="flex flex-col gap-6 p-8 items-center md:max-w-[70%]  lg:max-w-[35%]">
+        {movieCard}
+      </div>
     </>
   );
 };
